5bc83c109a72f1cf74e7796d7f5e10a9898afeee
adammmusial <musialadam83@gmail.com>
2024-03-25 17:43:19 +0800

    feat: add addBooktoLibrary function

--- index.js
+++ index.js
@@ -13,14 +13,3 @@ function addBookToLibrary(title,author,genre,read) {
 }
 
 
-function displayBooks(){
-    const bookGrid = document.getElementById('bookGrid')
-    bookGrid.innerHTML = "";
-
-    myLibrary.forEach(book => {
-        const card = document.createElement('div')
-        card.classList.add('card')
-        card.innerHTML = `<h3>${book.title}</h3><p>${book.author}</p><p>${book.genre}</p>`;
-        bookGrid.appendChild(card);
-    })
-}
