706ed24cb90467032d9e7564c515abc954ca79d3
adammmusial <musialadam83@gmail.com>
2024-03-25 17:44:41 +0800

    feat: add displayBooks function to loop over array

--- index.js
+++ index.js
@@ -12,4 +12,16 @@ function addBookToLibrary(title,author,genre,read) {
     myLibrary.push(newBook)
 }
 
+function displayBooks(){
+    const bookGrid = document.getElementById('bookGrid')
+    bookGrid.innerHTML = "";
+
+    myLibrary.forEach(book => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        card.innerHTML = `<h3>${book.title}</h3><p>${book.author}</p><p>${book.genre}</p>`;
+        bookGrid.appendChild(card);
+    })
+}
+
 
