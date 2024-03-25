49790d64bfd134f466d0a6494dfe1b35280437e4
adammmusial <musialadam83@gmail.com>
2024-03-25 18:07:57 +0800

    feat: add Eventlisteners

--- index.js
+++ index.js
@@ -25,3 +25,26 @@ function displayBooks(){
 }
 
 
+document.getElementById('addBookBtn').addEventListener('click', function () {
+    document.getElementById('bookDialog').showModal();
+})
+
+document.getElementById('closeDialog').addEventListener('click', function () {
+    document.getElementById('bookDialog').close();
+})
+
+document.getElementById('bookForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const genre = document.getElementById('genre').value;
+    const read = document.getElementById('read').value;
+
+    addBookToLibrary(title,author,genre,read);
+    displayBooks()
+    document.getElementById('bookDialog').close()
+    document.getElementById('bookForm').reset()
+})
+
+document.addEventListener('DOMContentLoaded',displayBooks)
